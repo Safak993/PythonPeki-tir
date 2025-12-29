5cc1a11ac55e61774d28b6fe35709d45dbd4f7a4
Safak993 <miracefendiyilmaz92371@gmail.com>
2025-12-29 19:26:15 +0300

    Videolar için kodlar

---
+++ Mini kodlar serisi #3 Matematik operatörleri.py
@@ -0,0 +1,33 @@
+#----Matematik Operatörleri----#
+#----\n----#
+# print("\nSelam\n-----\n-----")
+#----Matematik op. Devam----#
+# * = çarpma
+# + = Toplama
+# - = Çıkarma
+# / = Bölmek
+# // = tambölme
+#----Toplama----#
+# a = 7
+# b = 4
+# print(f"{b + a}")
+#----Çıkarma(fark)----#
+# a = 9
+# b = 14
+# print(b - a)
+#----Çarpma----#
+# Mirac = 15
+# Mirac_2 = 10
+# print(Mirac * Mirac_2)
+#----Bölme----#
+# Safak = 90
+# Safak_2 = 180
+# print(Safak_2 / Safak)
+#----Tamlı bölme----#
+# degisken = 19
+# degisken_2 = 889
+# print(degisken_2 // degisken)
+#----Tam olmayan bölme----#
+# a = 19
+# s = 889
+# print(s / a)
