2cef90ea9f4162442d973c5b7b6c5c468d51e446
Safak993 <miracefendiyilmaz92371@gmail.com>
2025-12-30 18:39:08 +0300

    Videolar için kodlar

---
+++ Mini kodlar serisi #4 Fonksiyonlar.py
@@ -0,0 +1,22 @@
+#----Fonksiyonlar----#
+#----Toplama fonksiyonu----#
+def topla(t,s):
+    print(t + s)
+topla(10,92)
+#----Çarpma fonksiyonu----#
+def carpma(safak_1,safak_2):
+    print(safak_1 * safak_2)
+carpma(5,5)
+#----Bölme----#
+def bolme(mirac_1,mirac_2):
+    print(mirac_1 / mirac_2)
+bolme(50,150)
+#----Tamlı bölme----#
+def tamli_bolme(t,s):
+    print(t // s)
+tamli_bolme(90,40)
+#----Nerede kullanılır----#
+def carp(x,y):
+    print(x * y)
+carp(900,700)
+
